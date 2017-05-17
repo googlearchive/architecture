b44fd2affbbcdb76ae2c86a410ca352d1bac6e4f
Patrice Chalin <pchalin@gmail.com>
2017-05-17 06:15:12 -0400

    Sync with c6e36ab
    Synced with dart-lang/site-webdev master branch, commit c6e36ab:
    https://github.com/dart-lang/site-webdev/tree/c6e36abc78469d9d8d7af73849fcfe29572bc0f2/examples/ng/doc/architecture

--- lib/hero_list_component.dart
+++ lib/hero_list_component.dart
@@ -1,14 +1,15 @@
-import 'package:angular2/core.dart';
+import 'package:angular2/angular2.dart';
 
 import 'hero.dart';
 import 'hero_detail_component.dart';
 import 'hero_service.dart';
 
 @Component(
-    selector: 'hero-list',
-    templateUrl: 'hero_list_component.html',
-    directives: const [HeroDetailComponent],
-    providers: const [HeroService])
+  selector: 'hero-list',
+  templateUrl: 'hero_list_component.html',
+  directives: const [COMMON_DIRECTIVES, HeroDetailComponent],
+  providers: const [HeroService],
+)
 class HeroListComponent implements OnInit {
   List<Hero> heroes;
   Hero selectedHero;
